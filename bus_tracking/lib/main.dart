74eb0be44529864f428d7ea30d6542c3ef95ea66
Arvind Kumar M <200020008@iitdh.ac.in>
2022-09-29 22:29:32 +0530

    Tried app linking

--- bus_tracking/lib/main.dart
+++ bus_tracking/lib/main.dart
@@ -1,10 +1,17 @@
-import 'dart:js';
+import 'dart:async';
+import 'dart:io';
 
 import 'package:bus_tracking/pages/home.dart';
 import 'package:bus_tracking/pages/location.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:uni_links/uni_links.dart';
+
+
+
 
 void main() {
+  
   runApp(MaterialApp(
     initialRoute: "/",
     routes: {
