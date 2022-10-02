50c07b3a707a6d5643701333e4c60aa4adc1ed49
Anand <anandishegde@gmail.com>
2022-10-03 00:24:31 +0530

    asking permission for location

--- bus_tracking/lib/pages/home.dart
+++ bus_tracking/lib/pages/home.dart
@@ -3,8 +3,6 @@ import 'dart:convert';
 import 'dart:async';
 import 'package:geolocator/geolocator.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
@@ -15,10 +13,56 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  
+  DateTime lastSentTime = DateTime(0,0,0,0,0);
+  double lastLatitude = 0.0;
+  double lastLongitude = 0.0;
+
+  
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    // if (!serviceEnabled) {
+    //   // Location services are not enabled don't continue
+    //   // accessing the position and request users of the 
+    //   // App to enable the location services.
+    //   return Future.error('Location services are disabled.');
+    // }
+
+    permission = await Geolocator.checkPermission();
+    print(permission);
+    if (permission == LocationPermission.denied) {
+
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale 
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+    
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately. 
+      return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+    } 
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
+  }
+
   // Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
   Future sendLocation() async {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
+    // Position position = await Geolocator.getCurrentPosition(
+    //     desiredAccuracy: LocationAccuracy.high);
+    Position position = await _determinePosition();
     var url = 'http://10.196.9.48:8080/api/location';
     int currentTime =  DateTime.now().millisecondsSinceEpoch;
     print("current real");
@@ -38,7 +82,19 @@ class _HomeState extends State<Home> {
         headers: {"Content-Type": "application/json"}, body: body);
     print("${response.statusCode}");
     print("${response.body}");
+
+
     if (response.statusCode == 200) {
+
+      setState(() {
+
+
+        lastSentTime = DateTime.fromMillisecondsSinceEpoch(currentTime);
+        lastLatitude = position.latitude;
+        lastLongitude = position.longitude;
+
+      });
+
       return json.decode(response.body);
     } else {
       print("Exception caught: Failed to get data");
@@ -49,7 +105,7 @@ class _HomeState extends State<Home> {
   void initState() {
     super.initState();
     // _locationController = StreamController();
-    Timer.periodic(Duration(seconds: 2), (_) => sendLocation());
+    Timer.periodic(Duration(milliseconds: 500), (_) => sendLocation());
   }
 
   @override
@@ -64,7 +120,13 @@ class _HomeState extends State<Home> {
                     await Navigator.pushNamed(context, "/location");
               },
               icon: Icon(Icons.location_city),
-              label: Text('Location'))
+              label: Text('Location')),
+
+              Text("Last sent time = "+lastSentTime.toString()),
+
+              Text("Last Latitude = "+lastLatitude.toString() ),
+              Text("Last Longitude ="+lastLongitude.toString())
+
         ],
       ),
     );
