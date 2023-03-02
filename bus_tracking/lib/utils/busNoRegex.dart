26e0da5df42ebbe9b133f39fd6096fc8a83c04d8
Anand <anandishegde@gmail.com>
2023-03-02 19:45:33 +0530

    code to make it capital

---
+++ bus_tracking/lib/utils/busNoRegex.dart
@@ -0,0 +1,12 @@
+// ignore: file_names
+import 'package:flutter/services.dart';
+
+class UpperCaseTextFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
+    return TextEditingValue(
+      text: newValue.text.toUpperCase(),
+      selection: newValue.selection,
+    );
+  }
+}
